463e6203a6779a4bbdfb523e762e15bb4aab3963
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 15:45:46 -0700

    working on catching exceptions; fixed counter

--- game.py
+++ game.py
@@ -14,23 +14,38 @@
 #         congratulate player
 
 from random import randint
-
+num = randint(1, 100)
+num_of_guesses = 0
 
 name = raw_input("Hello,what's your name? ")
 print "Welcome to the guessing game, %s." % (name)
-guess = int(raw_input("I'm thinking of a number between 1 and 100. Can you guess what it is? "))
 
-num_of_guesses = 1
 
-num = randint(1, 100)
+while True:
+    num_of_guesses += 1
+    try:
+        guess = int(raw_input("I'm thinking of a number between 1 and 100. Can you guess what it is? "))
+        break
+    except ValueError:
+        print "Do you know what a number is?! Try again."
+
 
 while guess != num:
     num_of_guesses += 1
-    if guess < 1 or guess > 100:
-        guess = int(raw_input("You guessed out of range! Please choose a number between 1 and 100. "))
-    elif guess < num:
-        guess = int(raw_input("Your guess is too low, try again. "))
-    elif guess > num:
-        guess = int(raw_input("Your guess is too high, try again. "))
+    # if guess < 1 or guess > 100:
+    #     guess = int(raw_input("You guessed out of range! Please choose a number between 1 and 100. "))
+    # elif guess < num:
+    #     guess = int(raw_input("Your guess is too low, try again. "))
+    # elif guess > num:
+    #     guess = int(raw_input("Your guess is too high, try again. "))
+    try:
+        if guess < 1 or guess > 100:
+            guess = int(raw_input("You guessed out of range! Please choose a number between 1 and 100. "))
+        elif guess < num:
+            guess = int(raw_input("Your guess is too low, try again. "))
+        elif guess > num:
+            guess = int(raw_input("Your guess is too high, try again. "))
+    except ValueError:
+        print "Do you know what a number is?! Try again."
 
 print "Well done, %s! You found my number in %s tries." % (name, num_of_guesses)
