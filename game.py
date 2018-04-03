fc0676e55ba0257b1c85ce8b667e4aa916e6c438
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 15:57:53 -0700

    fixed exception catching and counter.

--- game.py
+++ game.py
@@ -1,18 +1,5 @@
 """A number-guessing game."""
 
-# Put your code here
-
-# greet player
-# get player name
-# choose random number between 1 and 100
-# repeat forever:
-#     get guess
-#     if guess is incorrect:
-#         give hint
-#         increase number of guesses
-#     else:
-#         congratulate player
-
 from random import randint
 num = randint(1, 100)
 num_of_guesses = 0
@@ -31,21 +18,19 @@ while True:
 
 
 while guess != num:
-    num_of_guesses += 1
-    # if guess < 1 or guess > 100:
-    #     guess = int(raw_input("You guessed out of range! Please choose a number between 1 and 100. "))
-    # elif guess < num:
-    #     guess = int(raw_input("Your guess is too low, try again. "))
-    # elif guess > num:
-    #     guess = int(raw_input("Your guess is too high, try again. "))
-    try:
-        if guess < 1 or guess > 100:
-            guess = int(raw_input("You guessed out of range! Please choose a number between 1 and 100. "))
-        elif guess < num:
-            guess = int(raw_input("Your guess is too low, try again. "))
-        elif guess > num:
-            guess = int(raw_input("Your guess is too high, try again. "))
-    except ValueError:
-        print "Do you know what a number is?! Try again."
+    if guess < 1 or guess > 100:
+        print "You guessed out of range! Please choose a number between 1 and 100. "
+    elif guess < num:
+        print "Your guess is too low, try again. "
+    elif guess > num:
+        print "Your guess is too high, try again. "
+
+    while True:
+        num_of_guesses += 1
+        try:
+            guess = int(raw_input("Your guess?"))
+            break
+        except ValueError:
+            print "Do you know what a number is?! Try again."
 
 print "Well done, %s! You found my number in %s tries." % (name, num_of_guesses)
