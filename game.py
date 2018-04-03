8c1ee9bb17c1f681572282b5e2c77fe076eb02ef
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 15:19:24 -0700

    added range and fixed counter

--- game.py
+++ game.py
@@ -16,20 +16,25 @@
 from random import randint
 
 
-name = raw_input("Hello,what's your name?")
+name = raw_input("Hello,what's your name? ")
 print "Welcome to the guessing game, %s." % (name)
-guess = int(raw_input("I'm thinking of a number between 1 and 100. Can you guess what it is?"))
+guess = int(raw_input("I'm thinking of a number between 1 and 100. Can you guess what it is? "))
 
-num_of_guesses = 0
+num_of_guesses = 1
 
-num = randint(1,100)
+num = randint(1, 100)
+
+if guess < 1 or guess > 100:
+    guess = int(raw_input("You guessed out of range! Please choose a number between 1 and 100. "))
+    num_of_guesses += 1
 
 while guess != num:
-    if guess < num:
-        guess = int(raw_input("Your guess is too low, try again."))
-        num_of_guesses += 1
+    num_of_guesses += 1
+    if guess < 1 or guess > 100:
+        guess = int(raw_input("You guessed out of range! Please choose a number between 1 and 100. "))
+    elif guess < num:
+        guess = int(raw_input("Your guess is too low, try again. "))
     elif guess > num:
-        guess = int(raw_input("Your guess is too high, try again."))
-        num_of_guesses += 1
-    
+        guess = int(raw_input("Your guess is too high, try again. "))
+
 print "Well done, %s! You found my number in %s tries." % (name, num_of_guesses)
