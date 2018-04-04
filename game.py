557756eb1ad268f35cb29986accdcefb6a68e06b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 17:39:06 -0700

    added max guesses

--- game.py
+++ game.py
@@ -6,18 +6,23 @@ num_of_guesses = 0
 guess = 0
 answer = 'y'
 best_score = 1000
+max_guesses = 12
+guesses_left = max_guesses
 
-name = raw_input("Hello,what's your name? ")
+name = raw_input("Hello, what's your name? ")
 print "Welcome to the guessing game, %s. Guess a number between 1 and 100." % (name)
 
-while guess != num and answer == 'y':
+while guess != num and answer == 'y' and num_of_guesses < max_guesses:
+    print "You have %s guesses left." % (guesses_left)
     while True:
         num_of_guesses += 1
         try:
             guess = int(raw_input("Your guess?"))
             break
         except ValueError:
+            guesses_left = max_guesses - num_of_guesses
             print "Do you know what a number is?! Try again."
+            print "You have %s guesses left." % (guesses_left)
     if guess < 1 or guess > 100:
         print "You guessed out of range! Please choose a number between 1 and 100. "
     elif guess < num:
@@ -38,7 +43,9 @@ while guess != num and answer == 'y':
             num = randint(1, 100)
             num_of_guesses = 0
             guess = 0
+            guesses_left = max_guesses
             print "Guess a number between 1 and 100."
+    guesses_left = max_guesses - num_of_guesses
 
 print "Thanks for playing!"
 print "Your best score was %s tries!" % (best_score)
